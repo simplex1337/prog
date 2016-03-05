678fba4b519b5ed29062646d3448b800aa72f39d
simplex1337 <cas98@bk.ru>
2016-03-05 08:22:58 +0500

    update
    hz robit ili ne

--- anana.c
+++ anana.c
@@ -4,7 +4,7 @@
 #define MAXCNT 10
 #define MAXSIZE (MAXPATH*MAXCNT)
 
-void count();
+
 int main()
 {
 	char delim;
@@ -14,6 +14,24 @@ int main()
 	scanf("%c%*c",&delim);
 	printf("input paths: ");
 	fgets(paths,MAXSIZE,stdin);
+	i = stok(str, del, ptr);
+	
+	if( delim == ' '){
+		printf("delim is <space>\n");
+	}else
+		printf("delim is %c\n",delim);		
+	printf("path string is: %s\n",paths);
+
+}
+
+int schr(char *str, char delim)
+{
+	int i, idx = -1;
+	for(i = 0; (str[i] != '\0') && (str[i] != delim); i++);
+		if(str[i] == delim)
+			idx = i;
+return idx;
+}
 int stok(char str[], char delim, char *ptr[])
 {
 	char *suf = str;
@@ -27,13 +45,33 @@ int stok(char str[], char delim, char *ptr[])
 	}
 	return j;
 }
-	
-	if( delim == ' '){
-		printf("delim is <space>\n");
-	}else
-		printf("delim is %c\n",delim);		
-	printf("path string is: %s\n",paths);
-
+void suntok(char *str, char delim, char *ptr[], int cnt)
+{
+	int i;
+	for(i = 1; i < cnt; i++) {
+		*(ptr[i] - 1) = delim;
+	}
 }
-
-
+int sequal(char *s, char *s2)
+{
+	
+	int i, flg = 1;
+	
+	for(i = 0; out && (s[i] != '\0' && s2[i] != '\0'); i++) {
+		if (s[i] != s2[i]){
+			out = 0;
+			return flg;
+		}
+	}
+	return out;
+}
+int scspn(char str[], char nsym[])
+{
+	int i;
+	for(i = 0; str[i] != '\0'; i++)
+		if(schr(sym, str[i]) >= 0) {
+			break;
+		}
+	}
+	return i;
+}
